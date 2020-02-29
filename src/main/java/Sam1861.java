04835206a0f3b9211c9fb8ef339c996e6bbdebc6
brooke Jung <alice1994@nate.com>
2020-02-29 14:37:02 +0900

    Initial commit

---
+++ src/main/java/Sam1861.java
@@ -0,0 +1,5 @@
+public class Sam1861 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/Sam1949.java
@@ -0,0 +1,5 @@
+public class Sam1949 {
+    public static void main(String[] args) {
+
+    }
+}
