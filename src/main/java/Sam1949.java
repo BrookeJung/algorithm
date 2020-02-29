b74f71c7ac39cb80aa7dc98175c73c101b5c4e9e
brooke Jung <alice1994@nate.com>
2020-03-01 02:13:29 +0900

    1949 init

--- src/main/java/Sam1949.java
+++ src/main/java/Sam1949.java
@@ -4,7 +4,21 @@ import java.io.InputStreamReader;
 
 public class Sam1949 {
     static int[][] rand,visit; //visit 0 : 미방문 1 :방문
-    static int N,K;
+    static int N,K,L; //N:땅가로세로길이 , K:깍을수있는수치 ,L :최장경로길이
+    static int start; //시작높이
+    static int[] dx={1,-1,0,0}, dy={0,0,1,-1};
+
+    public static void searchPath(int x, int y, int leng ){
+
+        for(int i=0;i<4;i++){
+            int nx = x +dx[i];
+            int ny = y +dy[i];
+            if(nx>=0&&nx<4&&ny>=0&&ny<4){
+
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         int [] arr= new int[5];
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,6 +33,9 @@ public class Sam1949 {
                 String[] line = br.readLine().split(" ");
                 for(int j=0;j<N;j++){
                     rand[i][j]=Integer.parseInt(line[j]);
+                    if(start<rand[i][j]){
+                        start=rand[i][j];
+                    }
                 }
             }
 
