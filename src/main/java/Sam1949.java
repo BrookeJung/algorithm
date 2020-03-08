367289b4af4e1e62dda1bffb65e385e9bb6648ab
brooke Jung <alice1994@nate.com>
2020-03-08 23:46:20 +0900

    [Refactor]1949통과

--- src/main/java/Sam1949.java
+++ src/main/java/Sam1949.java
@@ -1,101 +1,78 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Sam1949 {
-    static int[][] rand, visit;
+    static int[][] rand, visited;
     static int N, K, MAX_LENG; //N:땅가로세로길이 , K:깍을수있는수치 ,L :최장경로길이
-    static int start; //시작높이
+    static int highest; //시작높이
     static int[] dx = {1, -1, 0, 0}, dy = {0, 0, 1, -1};
 
-    static class Peak {
-        int x, y, h, leng;
-
-        public Peak(int x, int y, int h, int leng) {
-            this.x = x;
-            this.y = y;
-            this.h = h;
-            this.leng = leng;
-        }
-
-        @Override
-        public String toString() {
-            return "(" + x + "," + y + ")" + h + "," + leng;
-        }
-    }
-
-    public static void searchPath(int x, int y, int leng, int[][] searchRand, int[][] visit) {
-        visit[x][y] = 1;
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
-                if (visit[nx][ny] == 1) {
-                    continue;
-                }
-                if (searchRand[nx][ny] < searchRand[x][y]) {
-                    searchPath(nx, ny, ++leng, searchRand, visit);
-                }
+    public static void search(int x, int y, int leng, Boolean isCutted) {
+        MAX_LENG = (MAX_LENG < leng) ? leng : MAX_LENG;
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+            if (nx < 0 || ny < 0 || nx >= N || ny >= N) {
+                continue;
             }
-            if (MAX_LENG < leng) {
-                MAX_LENG = leng;
+            if (visited[nx][ny] == 1) {
+                continue;
+            }
+            if (rand[x][y] <= rand[nx][ny]) {
+                if (!isCutted) {
+                    int goalCut = rand[nx][ny] - rand[x][y] +1;
+                    if (0 < goalCut && goalCut <= K) {
+                        int backup = rand[nx][ny];
+                        int cutter = rand[nx][ny] - goalCut;
+                        visited[nx][ny] = 1;
+                        rand[nx][ny] = cutter;
+                        search(nx, ny, leng + 1, true);
+                        rand[nx][ny] = backup;
+                        visited[nx][ny] = 0;
+                    }
+                }
+            } else {
+                visited[nx][ny] = 1;
+                search(nx, ny, leng + 1, isCutted);
+                visited[nx][ny] = 0;
             }
         }
+
     }
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine().trim());
         int printNum = 1;
         while (num-- > 0) {
-            String[] info = br.readLine().split(" ");
-            N = Integer.parseInt(info[0]);
-            K = Integer.parseInt(info[1]);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
             rand = new int[N][N];
+            visited = new int[N][N];
+            highest=0;
             for (int i = 0; i < N; i++) {
-                String[] line = br.readLine().split(" ");
+                st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < N; j++) {
-                    rand[i][j] = Integer.parseInt(line[j]);
-                    if (start < rand[i][j]) {
-                        start = rand[i][j];
-                    }
+                    rand[i][j] = Integer.parseInt(st.nextToken());
+                    highest = (highest < rand[i][j]) ? rand[i][j] : highest;
                 }
             }
-            ArrayList<Peak> peakArr = new ArrayList<>();
+            MAX_LENG = 0;
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    if (rand[i][j] == start) {
-                        peakArr.add(new Peak(i, j, start, 1));
+                    if (rand[i][j] == highest && visited[i][j] == 0) {
+                        visited[i][j] = 1;
+                        search(i, j, 1, false);
+                        visited[i][j] = 0;
                     }
                 }
             }
-            for (int p = 0; p < peakArr.size(); p++) {
-                visit = new int[N][N];
-                searchPath(peakArr.get(p).x, peakArr.get(p).y, 1, rand
-                        , visit);
-                for (int cut = 1; cut <= K; cut++) {
-                    for (int i = 0; i < N; i++) {
-                        for (int j = 0; j < N; j++) {
-                            visit = new int[N][N];
-                            int[][] cutRand = Arrays.copyOf(rand, rand.length);
-                            if(peakArr.get(p).x==i&&peakArr.get(p).y==j){
-                                continue;
-                            }
-                            cutRand[i][j] -= cut;
-                            searchPath(peakArr.get(p).x, peakArr.get(p).y, 1, cutRand, visit);
-                        }
-                    }
-                }
-            }
-
             System.out.println("#" + (printNum++) + " " + MAX_LENG);
-
-            MAX_LENG = 1;
-            start = 1;
-            peakArr.clear();
-
         }
     }
+
 }
